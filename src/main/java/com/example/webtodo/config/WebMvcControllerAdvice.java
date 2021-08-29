70bf51d39e84db71e8eefcf0ce77d5429fb17891
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-08-29 19:16:59 +0900

    WebMvcControllerAdvice.javaの追加

---
+++ src/main/java/com/example/webtodo/config/WebMvcControllerAdvice.java
@@ -0,0 +1,26 @@
+package com.example.webtodo.config;
+
+import com.example.webtodo.service.TaskNotFoundException;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
+
+
+@ControllerAdvice
+public class WebMvcControllerAdvice {
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+    }
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public String handleException(TaskNotFoundException e, Model model) {
+        model.addAttribute("message", e);
+
+        return "error/CustomPage";
+    }
+}
